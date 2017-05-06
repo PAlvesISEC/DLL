772fdb92f57393891c20502745a8a6eb1240a589
PAlvesISEC <paulompalves@gmail.com>
2017-05-06 18:17:48 +0100

    Inicio

---
+++ DLL/DLL.h
@@ -0,0 +1,38 @@
+//DLL.h
+// O bloco ifdef seguinte é o modo standard de criar macros que tornam a exportação de
+// funções e variáveis mais simples. Todos os ficheiros neste projeto DLL são
+// compilados com o símbolo DLL_IMP_EXPORTS definido. Este símbolo não deve ser definido
+// em nenhum projeto que use a DLL. Desta forma, qualquer outro projeto que inclua este
+// este ficheiro irá ver as funções e variáveis DLL_IMP_API como sendo importadas de uma
+// DLL.
+
+#include <windows.h>
+#include <tchar.h>
+
+//Definir uma constante para facilitar a leitura do protótipo da função
+//Este .h deve ser incluído no projeto que o vai usar (modo implícito)
+#define TAM 256
+
+//Esta macro é definida pelo sistema caso estejamos na DLL (<DLL_IMP>_EXPORTS definida)
+//ou na app (<DLL_IMP>_EXPORTS não definida) onde DLL_IMP é o nome deste projeto
+//#ifdef DLL_EXPORTS
+
+//**********************************************************************************
+#ifdef DLL_EXPORTS   // VEIO DE -> PROP PROJ -> C++ -> LINHA COMANDO   -->  Está lá o nome do proj + o EXPORT, copiar para aqui
+//**********************************************************************************
+
+#define DLL_IMP_API __declspec(dllexport)   // Deve ser acrescentado antes de cada declaração de função ****************
+#else										// ou var global a usar fora da DLL, é para exportar		****************
+#define DLL_IMP_API __declspec(dllimport)   // Se estiver na DLL faz o de sima, se não estiver faz este
+#endif
+
+//extern "C"   // Comentar para poder ser um ficheiro (.c) se não comentar tera de ser (.cpp)
+//{
+	//Variável global da DLL
+	extern DLL_IMP_API int nDLL;
+
+	extern DLL_IMP_API char ponteiro[40960];
+
+	//Funções a serem exportadas/importadas
+	DLL_IMP_API int UmaString(void);
+//}
